0abb960e4efac16b812c622f0a4f0784e2f0cbad
Osmar Queiroz <ramsosmar@gmail.com>
2015-12-17 22:24:27 -0200

    adicionando mais recurso de colisao

--- script/testComportamento.js
+++ script/testComportamento.js
@@ -1,22 +1,92 @@
 var corpo = new Corpo();
-corpo = ManipularDirecao.adicionarDirecao(corpo,new Direcao(1,0));
+corpo = ManipularDirecao.adicionarDirecao(corpo, new Direcao(1, 0));
 
-corpo = ManipularCorpo.adicionarComidaCorpo(corpo,MonipularComida.criarComida("feijao",1,1));
-corpo = ManipularCorpo.adicionarComidaCorpo(corpo,MonipularComida.criarComida("farofa",2,1));
-corpo = ManipularCorpo.adicionarComidaCorpo(corpo,MonipularComida.criarComida("frango",3,1));
-//QUnit.test("Corpo é objeto", function (assert) {
-//    assert.equal(typeof corpo, "object", "Objeto");
-//    assert.equal(corpo.posicao, null, "Posicao indefinida");
-//});
-//console.log(corpo.lSegmento)
-for( var i in corpo.lSegmento){
-    console.log(corpo.lSegmento[i].posicao);
-}
-console.log("--------------------")
-corpo = ManipularCorpo.processarDirecao(corpo);
-corpo = ManipularDirecao.adicionarDirecao(corpo,new Direcao(0,1));
-corpo = ManipularCorpo.processarDirecao(corpo);
-corpo = ManipularCorpo.processarDirecao(corpo);
-for( var i in corpo.lSegmento){
-    console.log(corpo.lSegmento[i].posicao);
-}
+corpo = ManipularCorpo.adicionarComidaCorpo(corpo, MonipularComida.criarComida("feijao", 2, 1));
+corpo = ManipularCorpo.adicionarComidaCorpo(corpo, MonipularComida.criarComida("farofa", 1, 1));
+
+QUnit.test("Lista com 2 segmentos", function (assert) {
+    var corpo = new Corpo();
+    corpo = ManipularDirecao.adicionarDirecao(corpo, new Direcao(1, 0));
+
+    corpo = ManipularCorpo.adicionarComidaCorpo(corpo, MonipularComida.criarComida("feijao", 2, 1));
+    corpo = ManipularCorpo.adicionarComidaCorpo(corpo, MonipularComida.criarComida("farofa", 1, 1));
+
+    assert.equal(corpo.lSegmento[0].posicao.x, 2, "Segmento 0 Posicao X = 2");
+    assert.equal(corpo.lSegmento[0].posicao.y, 1, "Segmento 0 Posicao Y = 1");
+
+    assert.equal(corpo.lSegmento[1].posicao.x, 1, "Segmento 1 Posicao X = 1");
+    assert.equal(corpo.lSegmento[1].posicao.y, 1, "Segmento 1 Posicao Y = 1");
+});
+
+
+QUnit.test("Lista com 2 segmentos movendo 2 passo", function (assert) {
+    var corpo = new Corpo();
+    corpo = ManipularDirecao.adicionarDirecao(corpo, new Direcao(1, 0));
+
+    corpo = ManipularCorpo.adicionarComidaCorpo(corpo, MonipularComida.criarComida("feijao", 2, 1));
+    corpo = ManipularCorpo.adicionarComidaCorpo(corpo, MonipularComida.criarComida("farofa", 1, 1));
+    corpo = ManipularCorpo.processarDirecao(corpo);
+    corpo = ManipularCorpo.processarDirecao(corpo);
+    assert.equal(corpo.lSegmento[0].posicao.x, 4, "Segmento 0 Posicao X = 4");
+    assert.equal(corpo.lSegmento[0].posicao.y, 1, "Segmento 0 Posicao Y = 1");
+
+    assert.equal(corpo.lSegmento[1].posicao.x, 3, "Segmento 1 Posicao X = 3");
+    assert.equal(corpo.lSegmento[1].posicao.y, 1, "Segmento 1 Posicao Y = 1");
+});
+
+QUnit.test("Lista com 2 segmentos movendo 2 passo direcao baixo", function (assert) {
+    var corpo = new Corpo();
+    corpo = ManipularDirecao.adicionarDirecao(corpo, new Direcao(1, 0));
+
+    corpo = ManipularCorpo.adicionarComidaCorpo(corpo, MonipularComida.criarComida("feijao", 2, 1));
+    corpo = ManipularCorpo.adicionarComidaCorpo(corpo, MonipularComida.criarComida("farofa", 1, 1));
+    corpo = ManipularCorpo.processarDirecao(corpo);
+    corpo = ManipularCorpo.processarDirecao(corpo);
+    corpo = ManipularDirecao.adicionarDirecao(corpo, new Direcao(0, 1));
+    corpo = ManipularCorpo.processarDirecao(corpo);
+    corpo = ManipularCorpo.processarDirecao(corpo);
+    
+    assert.equal(corpo.lSegmento[0].posicao.x, 4, "Segmento 0 Posicao X = 4");
+    assert.equal(corpo.lSegmento[0].posicao.y, 3, "Segmento 0 Posicao Y = 3");
+
+    assert.equal(corpo.lSegmento[1].posicao.x, 4, "Segmento 1 Posicao X = 4");
+    assert.equal(corpo.lSegmento[1].posicao.y, 2, "Segmento 1 Posicao Y = 2");
+});
+
+QUnit.test("Lista com 2 segmentos movendo 2 passo direcao baixo", function (assert) {
+    var corpo = new Corpo();
+    corpo = ManipularDirecao.adicionarDirecao(corpo, new Direcao(1, 0));
+
+    corpo = ManipularCorpo.adicionarComidaCorpo(corpo, MonipularComida.criarComida("feijao", 2, 1));
+    corpo = ManipularCorpo.adicionarComidaCorpo(corpo, MonipularComida.criarComida("farofa", 1, 1));
+    corpo = ManipularCorpo.processarDirecao(corpo);
+    corpo = ManipularCorpo.processarDirecao(corpo);
+    corpo = ManipularDirecao.adicionarDirecao(corpo, new Direcao(0,-1));
+    corpo = ManipularCorpo.processarDirecao(corpo);
+    corpo = ManipularCorpo.processarDirecao(corpo);
+    
+    assert.equal(corpo.lSegmento[0].posicao.x, 4, "Segmento 0 Posicao X = 4");
+    assert.equal(corpo.lSegmento[0].posicao.y, -1, "Segmento 0 Posicao Y = -1");
+
+    assert.equal(corpo.lSegmento[1].posicao.x, 4, "Segmento 1 Posicao X = 4");
+    assert.equal(corpo.lSegmento[1].posicao.y, 0, "Segmento 1 Posicao Y = 0");
+});
+
+QUnit.test("Lista com 2 segmentos movendo 2 passo direcao baixo", function (assert) {
+    var corpo = new Corpo();
+    corpo = ManipularDirecao.adicionarDirecao(corpo, new Direcao(-1, 0));
+
+    corpo = ManipularCorpo.adicionarComidaCorpo(corpo, MonipularComida.criarComida("feijao", 5, 4));
+    corpo = ManipularCorpo.adicionarComidaCorpo(corpo, MonipularComida.criarComida("farofa", 6, 4));
+    corpo = ManipularCorpo.processarDirecao(corpo);
+    corpo = ManipularCorpo.processarDirecao(corpo);
+    corpo = ManipularDirecao.adicionarDirecao(corpo, new Direcao(0,-1));
+    corpo = ManipularCorpo.processarDirecao(corpo);
+    corpo = ManipularCorpo.processarDirecao(corpo);
+    
+    assert.equal(corpo.lSegmento[0].posicao.x, 3, "Segmento 0 Posicao X = 3");
+    assert.equal(corpo.lSegmento[0].posicao.y, 2, "Segmento 0 Posicao Y = 2");
+
+    assert.equal(corpo.lSegmento[1].posicao.x, 3, "Segmento 1 Posicao X = 3");
+    assert.equal(corpo.lSegmento[1].posicao.y, 3, "Segmento 1 Posicao Y = 3");
+});
